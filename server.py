6ad0daab4f158b02a41a9ab6730f5abe12873144
ihooser <37125887+ihooser@users.noreply.github.com>
2018-03-21 16:39:33 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,73 @@
+'''
+Created on Mar 17, 2018
+
+@author: SYSTEM
+'''
+
+
+def outfile(data):
+    datafile = raw_input('Enter the file to write to:') 
+    if os.path.isfile(datafile):
+        print 'this file already exists'
+        choose = raw_input('would you like to over write (Y/N):')
+        if choose == "y" or choose== "Y":
+            date = open(datafile,'w')
+            date.write(data)
+            date.close
+            return  
+        else:
+            outfile(data)
+    else:
+        date = open(datafile,'w')
+        date.write(data)
+        date.close
+        return
+
+
+import re
+import socket
+import sys
+import os
+if __name__ == '__main__':
+    pass
+data = ""
+UDP_PORT_NO = int(sys.argv[1])
+print " the server port is: ", UDP_PORT_NO
+
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#getting the ip of the Server
+sock = socket.gethostbyname(socket.gethostname())
+UDP_IP_ADDRESS = sock
+print sock
+#binding together the ip and the port for this server
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+while True:
+    serverSock.listen(2)
+    print "_______________________________"
+    print "WAITING for client"
+    connection, addr = serverSock.accept()
+    print "Would you like to recive a file from"
+    print addr
+    choose = raw_input("(Y/N):")
+    if choose == "y" or choose== "Y":
+        while 1:
+            datain = connection.recv(1024)
+    
+            data = data + str(datain)
+            print data
+            return_message = "The data has fully been sent"
+            connection.send(return_message)
+            if not datain:
+                outfile(data)
+    #             serverSock.connect(connection)
+    #             return_message = "The data has fully been sent"
+    #             serverSock.send(return_message)
+                print "Recived all data"
+    #             return_message = "The data has fully been sent"
+    #             connection.send(return_message)
+                break
+        else: break
+       
+    
+
---
+++ tester.py
@@ -0,0 +1,57 @@
+'''
+Created on Mar 17, 2018
+
+@author: SYSTEM
+'''
+
+
+import sys
+import socket
+import os
+
+if __name__ == '__main__':
+    pass
+
+s=socket.socket (socket.AF_INET, socket.SOCK_DGRAM,0)
+#reading in all the imputs
+serverIP = sys.argv[1]
+serverPort = int(sys.argv[2])
+datafile = sys.argv[3]
+byte_reading= int(sys.argv[4])
+#choosing if data is from keyboard or file
+
+print "The server IP is:", serverIP
+print "The server Port is:", serverPort
+
+
+count = 0
+#this is the file reading form
+date = open(datafile,'rb')
+#getting the size of the file in bytes
+imformation = os.stat(datafile)
+Size_of_file = imformation.st_size
+print "The size of the file is: ", Size_of_file
+#to know how many times to send the data chunks
+counter_size = Size_of_file/byte_reading
+remain = Size_of_file%byte_reading
+print "the counter should go to: ",counter_size
+#setting up the socket
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSock.connect((serverIP, serverPort))
+while date: 
+    datain =date.read(byte_reading)
+    clientSock.send(datain)
+#     print datain
+    count = count + 1
+    if count == counter_size:
+        datain =date.read(remain)
+        clientSock.send(datain)
+#         print datain
+#         print "sent all data"
+        
+        data_back = clientSock.recv(1024)
+        print data_back
+        
+        break
+
+clientSock.close()
